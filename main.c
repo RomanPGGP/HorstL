eb967b86458722b6f3728484dd8dfc3655294f53
Roman <RomanPGGP>
2017-04-26 09:33:56 -0500

    pb

--- main.c
+++ main.c
@@ -635,6 +635,8 @@ int main(int argc, char** argv)
 	list_head_init(&nodes);
 	init_spectrum();
 
+	printf(" argc: %d\n", argc);
+
 	config_parse_file_and_cmdline(argc, argv);
 
 	sigint_action.sa_handler = sigint_handler;
