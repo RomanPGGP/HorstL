a4f4fcb37b7710d2bf43bca343b14945e78d9b33
Roman <RomanPGGP>
2017-05-03 11:17:55 -0500

    mn

--- main.c
+++ main.c
@@ -224,6 +224,7 @@ static void write_to_file(struct packet_info* p)
     char *mac;
     char *state;
     int cont = 0;
+    int stts=0;
 	//------------------
 
 
@@ -280,8 +281,7 @@ static void write_to_file(struct packet_info* p)
                         if(chq != NULL)
                         {
                         	fprintf(DF, "FOUND--->%s y %s\n ", readmac, mac);
-                        	return;
-                        	printf(DF, "SALIDA\n");
+                        	stts=1;
                         }
                         else
                         {
@@ -289,11 +289,14 @@ static void write_to_file(struct packet_info* p)
                         }        
                 }
         	}
-        	fprintf(DF, "%s, ", buf);
-            fprintf(DF, "%s, %s, ",
-			get_packet_type_name(p->wlan_type), ether_sprintf(p->wlan_src));
-			fprintf(DF, " %d \n",p->phy_signal);
-        	fseek(BLF,0, SEEK_SET);
+        	if(stts==0)
+        	{
+	        	fprintf(DF, "%s, ", buf);
+	            fprintf(DF, "%s, %s, ",
+				get_packet_type_name(p->wlan_type), ether_sprintf(p->wlan_src));
+				fprintf(DF, " %d \n",p->phy_signal);
+	        	fseek(BLF,0, SEEK_SET);
+        	}
 		}
 	}
 	
