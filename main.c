8ed9f2d05a647311568b43ed061b09e813f17f6e
Roman <RomanPGGP>
2017-05-03 11:45:12 -0500

    fnl

--- main.c
+++ main.c
@@ -282,29 +282,20 @@ static void write_to_file(struct packet_info* p)
                         chq = strstr(mac, readmac);
                         if(chq != NULL)
                         {
-                        	fprintf(DF, "FOUND--->%s y %s\n ", readmac, mac);
-                        	stts=1;
-                        }
-                        else
-                        {
-                        	fprintf(DF, "NOT F -> %s - %s\n", readmac, mac);
-                        }        
+                        	goto finale;
+                        }       
                 }
         	}
         	fseek(BLF,0, SEEK_SET);
-        	if(stts==0)
-        	{
-	        	fprintf(DF, "%s, ", buf);
-	            fprintf(DF, "%s, %s, ",
+	        fprintf(DF, "%s, ", buf);
+	        fprintf(DF, "%s, %s, ",
 				get_packet_type_name(p->wlan_type), ether_sprintf(p->wlan_src));
-				fprintf(DF, " %d \n",p->phy_signal);
-	        	fflush(DF);
-        	}
+			fprintf(DF, " %d \n",p->phy_signal);
+	        fflush(DF);
 		}
 	}
 	
-	
-	
+	finale:
 }
 
 /* return true if packet is filtered */
